4c3afbf8c668cb70259a5fe464f27b9805dac84f
samy023 <154593266+samy023@users.noreply.github.com>
2025-02-10 14:24:44 +0530

    Lec-5

---
+++ Lec-5/string.js
@@ -0,0 +1,15 @@
+let course="red and withe ";
+let count=0;
+let n=course.length;
+
+for(let i=0; i<n; i++){
+
+    if(course[i]== 'a'||course[i]== 'e' || course[i]== 'i'|| course[i]== 'o'|| course[i]== 'u')
+    {
+       count++;
+        
+    }
+    
+    
+}
+console.log(count);
